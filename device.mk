380af4b691851dc6e47093a3b93b566f33d278d0
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 08:59:13 +0300

    violet: Drop libvulkan target that is built by default
    Change-Id: Ic5d73508b850fac7c7427caf12eb4a8e87e3deca

--- device.mk
+++ device.mk
@@ -185,7 +185,6 @@ PRODUCT_PACKAGES += \
     gralloc.qcom \
     hwcomposer.qcom \
     libtinyxml \
-    libvulkan \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.memtrack-service
 
