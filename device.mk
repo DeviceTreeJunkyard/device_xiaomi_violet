eae528331a6c8e9f192180b416f4f7e2b59b47b3
imranpopz <imranpopz32@gmail.com>
2025-12-30 06:13:26 +0000

    violet: Build libjxl from source.
    * libjxl.so needed by camera lib libdng_sdk.so

--- device.mk
+++ device.mk
@@ -150,6 +150,7 @@ $(call soong_config_set_bool,camera,override_format_from_reserved,true)
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libjxl.vendor \
     libc29.vendor \
     libc++.vendor \
     libdemangle.vendor \
